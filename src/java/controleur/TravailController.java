5b8d03efce56c019460f083ad13e0a037b28fe23
Fara0597 <83493377+Fara0597@users.noreply.github.com>
2021-05-06 19:53:00 +0200

    TravailController.java
    update

--- src/java/controleur/TravailController.java
+++ src/java/controleur/TravailController.java
@@ -27,11 +27,12 @@ public class TravailController {
        int rs=0;
         try {
             Connection con = ConnectDB.connect();
-            PreparedStatement state = con.prepareStatement("INSERT INTO `travail` (`idTravail`,`numEmploye`, `numEntreprise`, `nbHeure`) VALUES (?, ?, ?, ?);");
+            PreparedStatement state = con.prepareStatement("INSERT INTO `travail` (`idTravail`, `numEmploye`, `numEntreprise`, `date`, `nbHeure`) VALUES (?, ?, ?,STR_TO_DATE(?, '%d/%m/%Y'),?)");
             state.setString(1, trav.getidTravail());
             state.setString(2, trav.getnumEmploye());
             state.setString(3, trav.getnumEntreprise());
-            state.setInt(4, trav.getnbHeure());
+            state.setString(4, trav.getdate());
+            state.setInt(5, trav.getnbHeure());
             rs = state.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -43,14 +44,15 @@ public class TravailController {
         JSONArray resultat = new JSONArray();
         try {
             Connection con = ConnectDB.connect();
-            PreparedStatement state = con.prepareStatement("SELECT * FROM travail");
+            PreparedStatement state = con.prepareStatement("SELECT idTravail, numEmploye, numEntreprise, DATE_FORMAT(date, \"%d/%m/%Y\"), nbHeure FROM travail");
             ResultSet rs = state.executeQuery();
             while (rs.next()){
             	JSONObject object=new JSONObject();
             	object.put("idTravail", rs.getInt(1));
             	object.put("numEmploye", rs.getString(2));
             	object.put("numEntreprise", rs.getInt(3));
-                object.put("nbHeure", rs.getInt(4));
+                object.put("date", rs.getString(4));
+                object.put("nbHeure", rs.getInt(5));
             	resultat.put(object);
             }
         } catch (SQLException e) {
@@ -63,14 +65,15 @@ public class TravailController {
        JSONObject object=new JSONObject();
         try {
             Connection con = ConnectDB.connect();
-            PreparedStatement state = con.prepareStatement("SELECT * FROM travail where idTravail = ?");
+            PreparedStatement state = con.prepareStatement("SELECT idTravail, numEmploye, numEntreprise, DATE_FORMAT(date, \"%d/%m/%Y\"), nbHeure FROM travail WHERE idTravail = ?");
             state.setInt(1,id);
             ResultSet rs = state.executeQuery();
             if (rs.next()){
             	object.put("idTravail", rs.getInt(1));
             	object.put("numEmploye", rs.getString(2));
             	object.put("numEntreprise", rs.getInt(3));
-                object.put("nbHeure", rs.getInt(4));
+                object.put("date", rs.getString(4));
+                object.put("nbHeure", rs.getInt(5));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -82,10 +85,11 @@ public class TravailController {
         int rs=0;
         try {
             Connection con = ConnectDB.connect();
-            PreparedStatement state = con.prepareStatement("UPDATE `travail` SET `idTravail` = ?,`numEmploye` = ?,`numEntreprise` = ?, `nbHeure` = ? WHERE `travail`.`idTravail` = ?;");
-            state.setString(1, trav.getidTravail());
-            state.setString(2, trav.getnumEmploye());
-            state.setString(3,trav.getnumEntreprise());
+            PreparedStatement state = con.prepareStatement("UPDATE `travail` SET `numEmploye` = ?,`numEntreprise` = ?, `date`= STR_TO_DATE(?, '%d/%m/%Y'), `nbHeure` = ? WHERE `travail`.`idTravail` = ?");
+           // state.setString(1, trav.getidTravail());
+            state.setString(1, trav.getnumEmploye());
+            state.setString(2,trav.getnumEntreprise());
+            state.setString(3,trav.getdate());
             state.setInt(4,trav.getnbHeure());
             state.setInt(5,id);
             rs = state.executeUpdate();
